cd4f95ee8ec1bbc0b3af671537ed6d0ddc4053a1
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2021-04-29 22:45:44 +0200

    Fix api path for vacation mode.

--- lib/my_zwave_api/vacation_mode.ex
+++ lib/my_zwave_api/vacation_mode.ex
@@ -1,12 +1,12 @@
 defmodule VacationMode do
   def get_state do
-    response = HTTPoison.get!(base_url)
+    response = HTTPoison.get!("/vacation_mode")
 
     Poison.decode(response.body)
   end
 
   def enable(start_time, end_time) do
-    url = "#{base_url}/on/#{start_time}/#{end_time}"
+    url = "/vacation_mode/on/#{start_time}/#{end_time}"
 
     response = HTTPoison.post!(url, "")
 
@@ -14,14 +14,10 @@ defmodule VacationMode do
   end
 
   def disable do
-    url = "#{base_url}/off"
+    url = "/vacation_mode/off"
 
     response = HTTPoison.post!(url, "")
 
     Poison.decode(response.body)
-  end
-
-  def base_url do
-    "http://#{Application.fetch_env!(:my_zwave_api, :driver_host)}/vacation_mode"
   end
 end
