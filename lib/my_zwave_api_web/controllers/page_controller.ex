468342f6ad66d676d8893ac58b51be32b43729b0
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2021-05-03 22:27:22 +0200

    Remove unnecessary page_view render.

--- lib/my_zwave_api_web/controllers/page_controller.ex
+++ lib/my_zwave_api_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule MyZwaveApiWeb.PageController do
   use MyZwaveApiWeb, :controller
 
   def csrf_token(conn, _params) do
-    render(conn, "csrf_token.json", csrf_token: get_csrf_token())
+    json(conn, %{csrf_token: get_csrf_token()})
   end
 end
--- lib/my_zwave_api_web/views/page_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule MyZwaveApiWeb.PageView do
-  use MyZwaveApiWeb, :view
-
-  def render("csrf_token.json", %{csrf_token: csrf_token}) do
-    %{
-      csrf_token: csrf_token
-    }
-  end
-end
